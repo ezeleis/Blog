5595b025bba799118d36d51eebd17daa58c058a6
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-03-21 16:04:42 -0300

    feat(blog): fetch blog data, load html, styled in cards

--- home.js
+++ home.js
@@ -1,15 +1,26 @@
 async function blogFetch(){ 
     await fetch ("https://jsonplaceholder.typicode.com/posts?_page=1&_limit=20")
-        .then((response)=>{
-            console.log(1,response);
-            return response.json();
-        })
-        .then((Json)=>{ 
-            let blog=document.createElement("div");
-            blog.innerHTML = Json;
-            document.body.appendChild(blog);
+        .then((json)=>{
+            console.log(1,json);
+            return json.json();
+        }).then((posts)=>{
+            console.log(2,posts);
+            posts.forEach(createPost);
         });
-    
 }
 
 
+function createPost(postObject){
+postUser = document.createElement('span');
+postUser.innerHTML = postObject.id;
+postTitle=document.createElement('h1');
+postTitle.innerHTML = postObject.title;
+postBody=document.createElement('p');
+postBody.innerHTML = postObject.body;
+post = document.createElement('div');
+post.className='post';
+post.appendChild(postUser);
+post.appendChild(postTitle);
+post.appendChild(postBody);
+document.getElementById('blogContainer').appendChild(post);
+};
