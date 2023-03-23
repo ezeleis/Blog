64e849af9b49c30f9b5763b650b63e25c52934ee
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-03-23 19:36:30 -0300

    get comments

--- home.js
+++ home.js
@@ -1,44 +1,68 @@
-let postCount=0;
+let fetchCount = 0;
+let currentPost;
 async function blogFetch(){ 
-    await fetch (`https://jsonplaceholder.typicode.com/posts?_page=1&_limit=20`)
+    let postsPage = fetchCount+1;
+    let postsLimit = 20;
+    let postCount = 0;
+    fetchCount ++;
+    // console.log(2,page);
+    await fetch (`https://jsonplaceholder.typicode.com/posts?_page=${postsPage}&_limit=${postsLimit}`)
         .then((json)=>{
             return json.json();
         }).then((posts)=>{
             posts.forEach(createPost);
-        });
+            posts.forEach(()=>postCount++);
+            
+        })
+        if (postCount>0){
+            document.getElementById('load-more-button').style.display='block';
+        }else document.getElementById('load-more-button').style.display='none';
 }
 
 function createPost(postObject){
-
+    
+    // console.log(currentPost);
+    let id = postObject.id;
     postUser = document.createElement('span');
-    postUser.innerHTML = postObject.id;
-
+    postUser.innerHTML = id;
+    
+    let title = postObject.title;
     postTitle=document.createElement('h1');
     postTitle.innerHTML = postObject.title;
 
+
+    let body = postObject.body;
     postBody=document.createElement('p');
     postBody.innerHTML = postObject.body;
 
     post = document.createElement('div');
     post.className='post';
 
+    let jsonObject = JSON.stringify(postObject); 
+    
+    post.setAttribute('onclick', `setCurrentPost(${jsonObject})`);
+
     post.appendChild(postUser);
     post.appendChild(postTitle);
     post.appendChild(postBody);
 
-    postCount ++;
-    console.log(1, postCount);
     document.getElementById('blogContainer').appendChild(post);
+    
     
 };
 
-async function loadMore(){
-    let page = postCount/20+1;
-    console.log(2,page);
-    await fetch (`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=20`)
-        .then((json)=>{
-            return json.json();
-        }).then((posts)=>{
-            posts.forEach(createPost);
-        })
+function setCurrentPost(postObject){
+   currentPost={"id":postObject.id, "title":postObject.title, "body":postObject.body};
+   console.log(currentPost);
+}
+
+async function getComments(postId){
+    await fetch (`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+    .then((comments)=>{
+        return comments });
 };
+
+function comments(id,title,body){
+    let comments=getComments(id)
+    currentPost = {"id":id,"title":title,"body":body,"comments":comments};
+}
